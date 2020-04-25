3657fa93a9829f98601bd915cb4cde7cb7295232
breezy-desk-5482 <rmcclorey1@gmail.com>
2020-04-25 01:04:24 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -3,7 +3,7 @@ def find_element_index(array, value_to_find)
   found_value_index = nil
   while num < array.length do
     if array[num] == value_to_find
-      found_value_index = count
+      found_value_index = num
     end
   num += 1
   end
